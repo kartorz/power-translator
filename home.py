699349153c914f2f965b22b590712a58c4073e60
joni.lee <joni.kartorz.lee@gmail.com>
2013-03-24 15:10:26 +0800

    Showing html

---
+++ home.py
@@ -0,0 +1,12 @@
+import webapp2
+
+import htmlsource
+
+class HomePage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['content-type'] = 'text/html'
+
+        self.response.write(htmlsource.HtmlSourceEn.strHtmlHome)
+
+app = webapp2.WSGIApplication([('/',HomePage)],debug=True)
+
--- translator.py
+++
@@ -1,8 +0,0 @@
-import webapp2
-
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['content-type'] = 'text/plain'
-        self.response.write('Hello, webapp2 world')
-
-app = webapp2.WSGIApplication([('/',MainPage)],debug=True)
